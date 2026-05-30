d3ce8d355f8845cf2bf1f5b01da9068b1326981c
codewithEshaYoutube <157880759+codewithEshaYoutube@users.noreply.github.com>
2026-05-30 20:43:21 +0500

    deployment

--- caresync/vite.config.js
+++ caresync/vite.config.js
@@ -4,11 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base : process.env.VITE_BASE_PATH || "/CareSync-AI",
   server: {
     proxy: {
       '/api': {
         target: 'http://localhost:8000',
         changeOrigin: true,
+
       },
     },
   },
